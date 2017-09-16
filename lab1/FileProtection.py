fe8078084b469d3bea494a5abb7129f3f275d404
robert <robert.otherone@gmail.com>
2017-09-15 22:32:41 -0400

    Finished with all steps File Protection and decryption

--- lab1/FileProtection.py
+++ lab1/FileProtection.py
@@ -1,4 +1,5 @@
 #Not Needed
+import sys
 from cryptography.fernet import Fernet
 import cryptography
 #Symmetric Key
@@ -11,11 +12,11 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.asymmetric import utils
 
 
-public_crypted_key = {}
-private_crypted_key = {}
-
-public_signed_key = {}
-private_signed_key = {}
+public_crypted_key = None
+private_crypted_key = None
+signature = None
+public_signed_key = None
+private_signed_key = None
 
 
 def new_keys():
@@ -39,9 +40,50 @@ def new_keys():
     print(private_signed_key)
     public_signed_key = private_signed_key.public_key()
 
-def sign_file():
-    message = (b"A message I need to sign")
+def sign_file(digest):
+    #Signing message
+    global signature
     signature = private_signed_key.sign(
+        digest,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    print("File is signed!")
+
+def hash_file(file_message):
+
+    message = file_message.encode(encoding='UTF-8')
+    chosen_hash = hashes.SHA256()
+    hasher = hashes.Hash(chosen_hash, default_backend())
+
+    #Hashing message
+    hasher.update(message)
+    digest = hasher.finalize()
+    print ("File is hashed! Here is hashed file:", digest)
+    #Call signing_message
+    sign_file(digest)
+    return digest
+
+def encryption(signed_file):
+    print sys.getsizeof(signed_file, "bytes")
+    cipertext = public_crypted_key.encrypt(
+        signed_file,
+        padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA1()),
+            algorithm=hashes.SHA1(),
+            label=None
+        )
+    )
+    print("File is completely encrypted! Here is encrypted File:", cipertext)
+    return cipertext
+
+def verify(message):
+    public_signed_key.verify(
+        signature,
         message,
         padding.PSS(
             mgf=padding.MGF1(hashes.SHA256()),
@@ -49,46 +91,8 @@ def sign_file():
         ),
         hashes.SHA256()
     )
-    print(signature)
+    print("The message matches the signature!")
 
-def hash_file():
-    chosen_hash = hashes.SHA256()
-    hasher = hashes.Hash(chosen_hash, default_backend())
-    hasher.update(b"file data")
-    digest = hasher.finalize()
-    signed = private_signed_key.sign(
-        digest,
-        padding.PSS(
-            mgf=padding.MGF1(chosen_hash),
-            salt_length=padding.PSS.MAX_LENGTH
-        ),
-        utils.Prehashed(chosen_hash)
-    )
-    print(signed)
-
-
-def signing_verifying_key(file_hash, pub_key):
-    key = Fernet.generate_key()
-    f = Fernet(key)
-    token = f.encrypt(b"my Deep dark secret")
-    print token
-
-    original_key = f.decrypt(token)
-    print original_key
-
-
-def encryption():
-    message = (b"encrypted data")
-    cipertext = public_crypted_key.encrypt(
-        message,
-        padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA1()),
-            algorithm=hashes.SHA1(),
-            label=None
-        )
-    )
-    print(cipertext)
-    return cipertext
 
 def decryption(cipertext):
     plaintext = private_crypted_key.decrypt(
@@ -99,24 +103,31 @@ def decryption(cipertext):
             label=None
         )
     )
-    print(plaintext)
+    print("File is completely decrypted! Here is decrypted file:", plaintext)
     return plaintext
 
 def read_file(filename):
     with open(filename, 'r') as f:
-        contents = f.readlines
-
-    print contents
+        contents = f.read().replace('\n', '')
+    #print contents
+    #print(type(contents))
+    print("File is loaded!")
+    return contents
 
 if __name__ == "__main__":
     print("Symmetric.py is being run directly")
-    read_file('testfile.txt')
-    new_keys()
-    sign_file()
-    hash_file()
 
-    print("encryption and decryption")
-    encrypted_msg = encryption()
-    dencrypted_msg = decryption(encrypted_msg)
+    new_keys()
+
+    file_txt = read_file('testfile.txt')
+    #sign_file(file_txt)
+    hashed_file = hash_file(file_txt)
+    verify(hashed_file)
+    print("Encryption and Decryption")
+    encrypted_msg = encryption(hashed_file)
+    decrypted_msg = decryption(encrypted_msg)
+    verify(decrypted_msg)
+    print("Here is decrypted_msg, it should be the same as the hashed_file", decrypted_msg)
+
 else:
     print("Symmetric.py is being imported")
