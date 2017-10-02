73c287e2d0eeecfdf4ede5e3bf0259ad0bf816af
robert <robert.otherone@gmail.com>
2017-10-02 19:19:38 -0400

    Finished 2 factor using twilio

---
+++ lab2/README.md
@@ -0,0 +1,12 @@
+Goal: Use 2-factor authentication and various cryptographic algorithms to establish a
+secure channel with a host site.
+Details: Login to a website using both a keyboard and a cell phone. Once the user clicks
+the login button / link at the website, the user is presented with a login screen. When the
+user enters his/her username and hits <return>, the website consults a file that it keeps
+locally to see which cell phone number is associated with that username. The website
+then sends an SMS text message to that phone number. The user, having received the
+text message, enters it and a password to a local application, which does some processing
+and returns a string. The user enters this string at the login screen and hits <return>. The
+website validates the sent value; if correct, it returns 2 strings which the user can input to
+the local application. The application validates these strings and returns success or fail to
+the user.
--- lab2/secure_channel.py
+++ lab2/secure_channel.py
@@ -1,8 +1,11 @@
 import sys
-import cryptography
 import json
+import twilio
+from twilio.rest import Client
 
+import yaml
 
+stored_data = {}
 
 def login(data):
     print (data)
@@ -18,25 +21,70 @@ def create_user(info):
 def send_beta():
     print ("beta")
 
-def compute_alpha(password):
+def compute_alpha():
+    password = "fdfdf"
+    password = stored_data['password']
     g = 9
     p = 23
-    return (g^len(password)) mod p
+    access_code = (g^len(password)) % p
+    print("Access code", access_code)
+    stored_data['accessCode'] = access_code
+    return (access_code)
 
 def get_info():
-    with open("login_info.json") as data_file:
-        data = json
+
+    global stored_data
 
     print ("Please enter your username!")
-    data[0]['userName'] = sys.stdin.readline()
+    data['userName'] = sys.stdin.readline()
 
     print ("Please enter your password!")
-    data[0]['password'] = sys.stdin.readline()
+    data['password'] = sys.stdin.readline()
 
-    print ("Please enter your phone number!")
-    data[0]['phoneNumber'] = sys.stdin.readline()
 
+
+def check_sms():
+    generated_value = "fd"
+
+    print ("Please enter your sms verification code!")
+    value = sys.stdin.readline()
+    if(value == generated_value):
+        print("You have logged in!")
+    else:
+        print("That is not your sms verification code!")
+
+
+
+def test_twilio():
+    data = {'account_sid': '',
+            'auth_token': ''}
+
+    with open("twilio_api_key.yaml", "r") as stream:
+        data = yaml.load(stream)
+
+    print(data["account_sid"])
+
+    client = Client(data["account_sid"], data["auth_token"])
+
+    access_code = stored_data['accessCode']
+    print("accessCode 123123123", stored_data)
+    print("accessCode ", stored_data['accessCode'])
+
+    try:
+        message = client.messages.create(
+
+            to="+16137934538",
+            from_="+13658006524",
+            body=access_code)
+
+        print(message.sid)
+    except twilio.base.exceptions.TwilioRestException as e:
+        print (e)
 
 if __name__ == "__main__":
 
     print ("Start!")
+    get_info()
+
+    compute_alpha()
+    test_twilio()
