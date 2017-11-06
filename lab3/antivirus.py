c457d3ecab500e42acb8405f785a9e3e8cec4e1e
Robert <roberthluo@users.noreply.github.com>
2017-11-06 11:42:03 -0500

    Update antivirus.py

--- lab3/antivirus.py
+++ lab3/antivirus.py
@@ -1,1 +1,19 @@
+import os
+import sys
 
+file_list = []
+
+
+def read_directory(root_path):
+  global file_list
+  for path, subdirs, files in os.walk(root_path):
+    for name in files:
+       print os.path.join(root_path, name)
+       file_path = os.path.join(root_path, name)
+       file_list.append(read_path) 
+
+
+
+
+if __name__ == "__main__":
+  read_directory("/directory")
